70a64360c843ac39784b0ff6c96ac7f550e90675
Shreyas Raj Singh <sxs170100@utdallas.edu>
2020-02-26 17:17:39 -0600

    tree helper methods set up

--- AVLTree.py
+++ AVLTree.py
@@ -1,41 +1,25 @@
+import AVLNode
+import Book
+
+
 class AVLTree(object):
 
-    def insert(self, root, key):
+    def getHeight(self, node):
+        if node is None:
+            return -1
 
-        # Step 1 - Perform normal BST
-        if not root: 
-            return (key) 
-        elif key < root.val: 
-            root.left = self.insert(root.left, key) 
-        else: 
-            root.right = self.insert(root.right, key) 
-  
-        # Step 2 - Update the height of the  
-        # ancestor node 
-        root.height = 1 + max(self.getHeight(root.left), 
-                           self.getHeight(root.right)) 
-  
-        # Step 3 - Get the balance factor 
-        balance = self.getBalance(root) 
-  
-        # Step 4 - If the node is unbalanced,  
-        # then try out the 4 cases 
-        # Case 1 - Left Left 
-        if balance > 1 and key < root.left.val: 
-            return self.rightRotate(root) 
-  
-        # Case 2 - Right Right 
-        if balance < -1 and key > root.right.val: 
-            return self.leftRotate(root) 
-  
-        # Case 3 - Left Right 
-        if balance > 1 and key > root.left.val: 
-            root.left = self.leftRotate(root.left) 
-            return self.rightRotate(root) 
-  
-        # Case 4 - Right Left 
-        if balance < -1 and key < root.right.val: 
-            root.right = self.rightRotate(root.right) 
-            return self.leftRotate(root) 
-  
-        return root
+        return node.height
+    
+    def getBalance(self, node):
+        if node is None:
+            return -1
+        
+        return self.getHeight(node.left_ptr) - self.getHeight(node.right_ptr)
+    
+    def InOrder(self, root):
+        if root is None:
+            return
+        
+        self.InOrder(root.left_ptr)
+        print("{0} ".format(root.val), end="")
+        self.InOrder(root.right_ptr)
